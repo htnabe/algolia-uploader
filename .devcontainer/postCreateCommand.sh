4d24aae681638d52944a80179d0548eabebe8550
htnabe <t_official@t-pot.me>
2025-02-28 14:12:47 +0900

    removed installation command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,1 +1,1 @@
-export COREPACK_ENABLE_DOWNLOAD_PROMPT=0 && sudo corepack enable && yarn set version stable && yarn install
+export COREPACK_ENABLE_DOWNLOAD_PROMPT=0 && sudo corepack enable && yarn set version stable
