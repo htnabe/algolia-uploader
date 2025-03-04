df11ff401b5e89afaf8e23c40f2607b1d14363a3
htnabe <t_official@t-pot.me>
2025-03-04 06:00:14 +0000

    moved isIndexedItem from uploader.ts to IndexedItem.ts

--- src/types/IndexedItem.ts
+++ src/types/IndexedItem.ts
@@ -2,3 +2,13 @@ export default interface IndexedItem {
   objectID: string;
   [key: string]: unknown;
 }
+
+// Guard the IndexedItem type
+export function isIndexedItem(item: unknown): item is IndexedItem {
+  return (
+    typeof item === "object" &&
+    item !== null &&
+    "objectID" in item &&
+    typeof (item as IndexedItem).objectID === "string"
+  );
+}
